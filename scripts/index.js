d7027efb8a2e2b2da26750fa333db887dbe023a2
Aliya N.A <aliya.anindita@taralite.com>
2018-08-02 17:46:28 +0700

    Fixed script

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,29 @@
 window.onload = function () {
-  var bigIconContainer = document.getElementById("big-icon");
-  var bigIcon = bigIconContainer.childNodes[0];
+  try {
+    var bigIconContainer = document.getElementById("big-icon");
+  }
+  catch (err) {
+    var bigIconContainer = false;
+  }
 
-  var entries = [{ id: "fictions-link", icon: "icon-book-alt2" }, { id: "writings-link", icon: "icon-pencil" }, { id: "translation-link", icon: "icon-earth" }, { id: "coding-link", icon: "icon-embed2" }];
+  if (bigIconContainer) {
+    var bigIcon = bigIconContainer.childNodes[0];
 
-  entries.forEach(function (entry) {
-    var link = document.getElementById(entry.id);
+    var entries = [{ id: "fictions-link", icon: "icon-book-alt2" }, { id: "writings-link", icon: "icon-pencil" }, { id: "translation-link", icon: "icon-earth" }, { id: "coding-link", icon: "icon-embed2" }];
 
-    link.addEventListener("mouseover", function () {
-      bigIconContainer.style.fontSize = "18vw";
-      bigIcon.className = entry.icon;
+    entries.forEach(function (entry) {
+      var link = document.getElementById(entry.id);
+
+      link.addEventListener("mouseover", function () {
+        bigIconContainer.style.fontSize = "18vw";
+        bigIcon.className = entry.icon;
+      });
+
+      link.addEventListener("mouseout", function () {
+        bigIconContainer.style.fontSize = "0";
+      });
     });
-
-    link.addEventListener("mouseout", function () {
-      bigIconContainer.style.fontSize = "0";
-    });
-  });
+  }
 
   var aboutButton = document.getElementById("about-button");
   var aboutText = document.getElementById("about-text");
